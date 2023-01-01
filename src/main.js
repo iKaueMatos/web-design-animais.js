962871a14c81a161daf74aa5cf64e6cb14bee1ef
ikauematos <kaue178hotimail@gmail.com>
2023-01-01 09:16:57 -0300

    atualização

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ const img = document.querySelector('img')
 img.addEventListener('click',()=>{
     console.log('clicou')
 })
+
+
+//Callback - É boa pratica separar a função de callback do addEventListener,ou seja,declarar uma 
+//função ao inves de passar diretamente uma função anonima
+
