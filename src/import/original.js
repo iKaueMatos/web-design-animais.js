8925d17b40ef070cbf73cbba2bd5d8e98c6945bd
ikauematos <kaue178hotimail@gmail.com>
2023-01-04 22:31:39 -0300

    atualização javascript

--- src/import/original.js
+++ src/import/original.js
@@ -1,15 +1,42 @@
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabcontent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(index) {
+// ForEach.section 
+    tabcontent.forEach ((content)=>{
+        content.classList.remove('ativo');
+    });
+    tabcontent[index].classList.add('ativo');
+}
+
+
+
+//exemploone.forEach((exemplo,){
+//exemplo.addEventListener('mouseouver')
+//
+//});
+
+tabmenu.forEach((itemMenu,index) => {
+    itemMenu,addEventListener('click',() => {
+        activeTab(index);
+
+    });
+});
 
 
 //Identificando as teclas que são clicadas
 
 function hadleKeyboard(event){
     if(event.key === "f11" ){
-        document.body.classList.toggle('fullscrenn')
-    }else if(event.key === 'b'){
-        document.body.classList.toggle('dark')
-       
+        document.body.classList.toggle('fullscrenn');
+    }else if(event.key === 'b'){    
+        document.body.style.color = '#fff';
+        document.body.style.background = '#222';
     }
+    
 }
 
-window.addEventListener('keydown',hadleKeyboard)
+window.addEventListener('keydown',hadleKeyboard) // window da possibilidade de acessarmos diferentes propriedades ou metofos(funções);
+
+
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-//inner HTML basicamente ele ira pegar o elemento sleecionado e ira altera-lo somente aquele elemento 
-//outerHTML ele ira de fato alterar o que esta dentro do nosso elemento selecionado e pegara os elementos relacionados 
 
-
-const h1 = document.querySelector('h1');
-h1.outerHTML = '<h1>Nunca nem vi</h1>';
 
