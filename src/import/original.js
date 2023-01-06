77301ef6efd0489a7eaa1c3436e229d2a96eb188
ikauematos <kaue178hotimail@gmail.com>
2023-01-06 06:55:19 -0300

    atualização

--- src/import/original.js
+++ src/import/original.js
@@ -1,42 +1,64 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
+
+//função 1
+const initTabNav = () => {
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
 
 function activeTab(index) {
-// ForEach.section 
-    tabcontent.forEach ((content)=>{
-        content.classList.remove('ativo');
+    tabContent.forEach((content) => {
+      content.classList.remove('ativo');
     });
-    tabcontent[index].classList.add('ativo');
-}
-
-
-
+    tabContent[index].classList.add('ativo');
+  }
 //exemploone.forEach((exemplo,){
 //exemplo.addEventListener('mouseouver')
 //
 //});
-
-tabmenu.forEach((itemMenu,index) => {
-    itemMenu,addEventListener('click',() => {
-        activeTab(index);
-
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+  
     });
-});
+  });
+}
+initTabNav();
 
-
+//Função 2
+const initkeyboard = () =>{
 //Identificando as teclas que são clicadas
-
 function hadleKeyboard(event){
     if(event.key === "f11" ){
         document.body.classList.toggle('fullscrenn');
-    }else if(event.key === 'b'){    
+    }else if(event.key === 'd'){    
         document.body.style.color = '#fff';
         document.body.style.background = '#222';
+    }else if (event.key === 'b'){
+        document.body.style.color ='#000';
+        document.body.style.background = '#fff';
     }
     
 }
-
 window.addEventListener('keydown',hadleKeyboard) // window da possibilidade de acessarmos diferentes propriedades ou metofos(funções);
 
+}
+initkeyboard()
 
+
+//função 3 
+const initaccordionteam = () =>{
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+
+  function activeAccordion(event){
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+  };
+  
+  accordionList.forEach((item) =>{
+  item.addEventListener('click',activeAccordion);
+
+  });
+};
+
+initaccordionteam()
 
